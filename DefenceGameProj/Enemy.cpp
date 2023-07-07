10cd4b8b04673c66c6d21daabc5d9008064cc519
clairhardt98 <woojae0107@gmail.com>
2023-07-07 17:51:34 +0900

    123

--- DefenceGameProj/Enemy.cpp
+++ DefenceGameProj/Enemy.cpp
@@ -9,6 +9,7 @@ Enemy::Enemy(const POINT& center)
 	:GameObject(center)
 {
 	Force = Vector2D(0, 5);
+	Radius = 10;
 	Health = 3;
 }
 
@@ -16,4 +17,9 @@ void Enemy::Update()
 {
 	Center.x += Force.getX();
 	Center.y += Force.getY();
+}
+
+void Enemy::Draw(HDC hdc)
+{
+	Ellipse(hdc, Center.x - Radius, Center.y - Radius, Center.x + Radius, Center.y + Radius);
 }
